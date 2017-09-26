449a1d3f93fab9aaf3100b9e1ac3c879666e53bc
Manish Pramod Patil <manish4487@gmail.com>
2017-09-26 17:23:34 -0500

    changes done for query added from database to check authentication

--- LogIn.aspx.cs
+++ LogIn.aspx.cs
@@ -1,5 +1,8 @@
- using System;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +12,9 @@ namespace MySQL_CRUD
 {
     public partial class LogIn : System.Web.UI.Page
     {
+        MySqlConnection conn = new
+        MySqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,10 +22,36 @@ namespace MySQL_CRUD
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
-            if (txtusername.Text.ToString() != "" && txtpassword.Text.ToString() != "")
-            {            
-                Response.Redirect("Student.aspx");
-            }            
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            try
+            {
+                if (txtusername.Text.ToString() != "" && txtpassword.Text.ToString() != "")
+                {
+                    MySqlDataReader read_rd;
+                    String selectquery = "Select * from login where username='" + txtusername.Text + "' and password='" + txtpassword.Text + "'";
+                    MySqlCommand cmd = new MySqlCommand(selectquery, conn);
+                     read_rd = cmd.ExecuteReader();
+                    if (read_rd.HasRows)
+                    {
+                        Response.Redirect("Student.aspx");
+                    }
+                    else {
+                        Response.Write("<script>alert('LogIn Failed');</script>");
+                    }
+                }
+            }
+            catch(MySqlException ex)
+            {
+                ShowMessage(ex.Message);
+            }
+        }
+
+        void ShowMessage(string msg)
+        {
+            ClientScript.RegisterStartupScript(Page.GetType(), "validation", "<script language = 'javascript' > alert('" + msg + "');</ script > ");
         }
 
     }
