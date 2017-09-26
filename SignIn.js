f8da234016dae90715989e1c8f72a571aff359e4
Manish Pramod Patil <manish4487@gmail.com>
2017-09-26 16:03:24 -0500

    Add files via upload

---
+++ SignIn.js
@@ -0,0 +1,16 @@
+﻿
+
+$(document).ready(function () {
+    $("#btnSubmit").click(function(){
+        var txtusername = $("#txtusername").val();
+        var txtpassword = $("#txtpassword").val();
+
+        // Checking for blank fields.
+        if (txtusername == '' || txtpassword == '') {
+            $('input[type="text"],input[type="txtpassword"]').css("border", "2px solid red");
+            $('input[type="text"],input[type="txtpassword"]').css("box-shadow", "0 0 3px red");
+
+            alert("Please fill all fields...!!!!!!");
+        }
+    });
+});
